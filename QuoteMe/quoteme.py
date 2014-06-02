cbd2452a1d581900ae246cd0f5304f0ea1912859
Voussoir <edalool@yahoo.com>
2014-06-02 12:50:55 -0700

    reddit 21

--- QuoteMe/quoteme.py
+++ QuoteMe/quoteme.py
@@ -69,7 +69,10 @@ def scanSub():
     sql.commit()
 
 while True:
-    scanSub()
+    try:
+        scan()
+    except Exception as e:
+        print('An error has occured:', e)
     print('Running again in ' + WAITS + ' seconds \n')
     sql.commit()
     time.sleep(WAIT)
