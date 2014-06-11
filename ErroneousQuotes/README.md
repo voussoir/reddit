d3c4b8e5f1134dc663b206089343c3274d68c884
Voussoir <edalool@yahoo.com>
2014-06-10 22:18:02 -0700

    reddit 35
    Introducing Erroneous Quotes!

---
+++ ErroneousQuotes/README.md
@@ -0,0 +1,15 @@
+ErroneousQuotes
+==========
+
+This bot will take phrases that people say and erroneously attribute them to famous historical figures.
+
+The **first** thing in a comment must be /u/Bots-Username. The bot will only respond if the requested quote is less than a certain number of characters.
+
+/u/GoldenSights Always be careful on the internet, kids!
+
+|
+V
+
+>Always be careful on the internet, kids!
+
+• Napoleon Bonaparte
---
+++ ErroneousQuotes/erroneousquotes.py
@@ -0,0 +1,83 @@
+#/u/GoldenSights
+import praw # simple interface to the reddit API, also handles rate limiting of requests
+import time
+import sqlite3
+import random
+
+'''USER CONFIGURATION'''
+
+USERNAME  = ""
+#This is the bot's Username. In order to send mail, he must have some amount of Karma.
+PASSWORD  = ""
+#This is the bot's Password. 
+USERAGENT = ""
+#This is a short description of what the bot does. For example "/u/GoldenSights' Newsletter bot"
+SUBREDDIT = "GoldTesting"
+#This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subreddits, use "sub1+sub2+sub3+..."
+NAMES = ["Abraham Lincoln", "George Washington", "Bill Gates", "Rosa Parks", "/u/GoldenSights", "/u/Unidan", "Napoleon Bonaparte"]
+#These are the words you are looking for
+MAXPOSTS = 100
+#This is how many posts you want to retrieve all at once. PRAW can download 100 at a time.
+MAXLENGTH = 150
+#To avoid bot abuse, do not generate any quotes longer than this many characters.
+WAIT = 20
+#This is how many seconds you will wait between cycles. The bot is completely inactive during this time.
+
+
+'''All done!'''
+
+
+
+
+WAITS = str(WAIT)
+try:
+    import bot #This is a file in my python library which contains my Bot's username and password. I can push code to Git without showing credentials
+    USERNAME = bot.getuG()
+    PASSWORD = bot.getpG()
+    USERAGENT = bot.getaG()
+except ImportError:
+    pass
+
+cutoff = len(USERNAME) + 4
+sql = sqlite3.connect('sql.db')
+print('Loaded SQL Database')
+cur = sql.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
+print('Loaded Completed table')
+
+sql.commit()
+
+r = praw.Reddit(USERAGENT)
+r.login(USERNAME, PASSWORD) 
+
+def scanSub():
+    print('Searching '+ SUBREDDIT + '.')
+    subreddit = r.get_subreddit(SUBREDDIT)
+    posts = subreddit.get_comments(limit=MAXPOSTS)
+    for post in posts:
+        pid = post.id
+        pbody = post.body
+        cur.execute('SELECT * FROM oldposts WHERE ID="%s"' % pid)
+        if not cur.fetchone():
+            cur.execute('INSERT INTO oldposts VALUES("%s")' % pid)    
+            if pbody.lower()[:cutoff] == '/u/' + USERNAME.lower() + ' ':
+                quote = pbody.split('\n\n')[0][cutoff:]
+                if len(quote) <= MAXLENGTH:
+                    name = NAMES[random.randint(0,len(NAMES)-1)]
+                    print(pid + ': ' + quote + '- ' + name)
+                    response = '>' + quote + '\n\n- ' + name
+                    post.reply(response)
+                else:
+                    print(pid + ': Comment too long')
+    sql.commit()
+
+while True:
+    try:
+        scanSub()
+    except Exception as e:
+        print('An error has occured:', str(e))
+    print('Running again in ' + WAITS + ' seconds \n')
+    sql.commit()
+    time.sleep(WAIT)
+ 
