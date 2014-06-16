f656f429f5ac9caa5775cf1ef9a9eb885d5e80f4
Voussoir <edalool@yahoo.com>
2014-06-15 23:24:26 -0700

    reddit 40
    Major NSAL work

--- Nsal/README.md
+++ Nsal/README.md
@@ -5,4 +5,4 @@ Written for /u/erktheerk
 
 Modification of SubDump. Searches for posts in designated subreddits with certain titles and urls. Dumps them into your own subreddit.
 
-Coming soon: Comment generator to show where else this link has been submitted to reddit.
+Links in a designated subreddit (Preferably the dumpsub) will undergo an Other Discussions process which finds other posts on reddit which have used the same submission.
--- Nsal/nsal.py
+++ Nsal/nsal.py
@@ -1,6 +1,7 @@
 #/u/GoldenSights
 import praw # simple interface to the reddit API, also handles rate limiting of requests
 import time
+import datetime
 import sqlite3
 
 '''USER CONFIGURATION'''
@@ -11,7 +12,7 @@ PASSWORD  = ""
 #This is the bot's Password. 
 USERAGENT = ""
 #This is a short description of what the bot does. For example "/u/GoldenSights' Newsletter bot"
-SUBREDDIT = "GoldTesting"
+SUBREDDIT = "Test"
 #This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subs, use "sub1+sub2+sub3+...". For all use "all"
 KEYWORDS = ["snowden", "greenwald", "nsa"]
 #Any comment containing these words will be saved.
@@ -19,12 +20,14 @@ KEYDOMAINS = ["theguardian.com", "techdirt.com"]
 
 SUBDUMP = True
 #Do you want the bot to dump into a subreddit as posts? Use True or False (Use capitals! No quotations!)
-DSUB = "Test"
+DSUB = "GoldTesting"
 #If SUBDUMP is set to True, you will need to choose a subreddit to submit to.
 ALLOWSELF = False
 #Do you want the bot to dump selfposts? Use True or False (Use capitals! No quotations!)
 DISTINGUISHPOST = False
 #Do you want the bot to mod-distinguish the post?
+DISTINGUISHCOMMENT = False
+#Do you want the bot to mod-distinguish the comment on the dump?
 TITLE = "_subreddit_: _ptitle_"
 #This is the title of the submission to your dump
 #Leave blank to keep the original post's title
@@ -32,6 +35,15 @@ TITLE = "_subreddit_: _ptitle_"
 #_subreddit_ = subreddit of original post
 #_author_ = OP of original post
 #_ptitle_ = Title of original post
+
+NSUB = "GoldTesting"
+#This is the subreddit that will undergo the Other Discussions test.
+DELAY = 10
+#How many SECONDS old must a post be on your sub before it receives an Other Discussion box?
+DISTINGUISHN = False
+#Do you want the bot to mod-distinguish the comment?
+HEADER = "**Other Discussions on reddit:**\n\nSubreddit | Author | Post | Time\n:- | - | - | -:\n"
+#This will be at the top of the Other Discussions comment. This formatting will prepare the box.
 
 
 MAXPOSTS = 100
@@ -66,6 +78,15 @@ sql.commit()
 
 r = praw.Reddit(USERAGENT)
 r.login(USERNAME, PASSWORD) 
+
+
+def getTime(bool):
+    timeNow = datetime.datetime.now(datetime.timezone.utc)
+    timeUnix = timeNow.timestamp()
+    if bool == False:
+        return timeNow
+    else:
+        return timeUnix
 
 def scanSub():
     print('Searching '+ SUBREDDIT + '.')
@@ -112,9 +133,51 @@ def scanSub():
         
     sql.commit()
 
+def discussions():
+    print('\nScanning ' + NSUB)
+    subreddit = r.get_subreddit(NSUB)
+    posts = subreddit.get_new(limit=MAXPOSTS)
+    for post in posts:
+        result = []
+        pid = post.id
+        cur.execute('SELECT * FROM oldposts WHERE ID=?', [pid])
+        if not cur.fetchone():
+            ptime = post.created_utc
+            curtime = getTime(True)
+            if curtime - ptime > DELAY:
+                purl = post.url
+                print(pid + ': Trying OD box')
+                search = r.search('url:"' + purl + '"', sort='new')
+                for item in search:
+                    if item.id != pid:
+                        timestamp = item.created_utc
+                        timestamp = datetime.datetime.fromtimestamp(int(timestamp)).strftime("%A %B %d, %Y %H:%M UTC")
+                        try:
+                            iauthor = '/u/' + item.author.name
+                        except AttributeError:
+                            pauthor = '[deleted]'
+                        subreddit = '/r/' + item.subreddit.display_name
+                        ilink = item.permalink
+                        ilink = ilink.replace('http://www', 'http://np')
+                        result.append(subreddit + ' | ' + iauthor + ' | [post](' + ilink + ') | ' + timestamp)
+                if len(result) > 0:
+                    final = HEADER + '\n'.join(result)
+                    if len(final) < 10000:
+                        print('\tCreating comment')
+                        post.add_comment(final)
+                else:
+                    print('\tNo results!')
+                cur.execute('INSERT INTO oldposts VALUES(?)', [pid])
+            else:
+                print(pid + ': Too young')
+
+
+
+
 while True:
     try:
         scanSub()
+        discussions()
     except Exception as e:
         print('An error has occured:', str(e))
     print('Running again in ' + WAITS + ' seconds \n')
