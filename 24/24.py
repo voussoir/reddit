227db295b14baf7a484904623d75f31d95352137
Voussoir <edalool@yahoo.com>
2014-07-28 16:22:30 -0700

    reddit 57
    Introducing PointsBot

--- 24/24.py
+++ 24/24.py
@@ -1,4 +1,5 @@
 #By /u/GoldenSights
+#Python 3.4.1
 import praw
 import configparser
 import sqlite3
@@ -15,22 +16,27 @@ sql = sqlite3.connect('sql.db')
 cur = sql.cursor()
 
 cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
+#Posts in here will never get scanned again
 cur.execute('CREATE TABLE IF NOT EXISTS requests(ID TEXT)')
 cur.execute('CREATE TABLE IF NOT EXISTS activedebates(ID TEXT, time TEXT, questions TEXT)')
+#Posts in here are active and will be repeatedly managed
 print('Loaded SQL Database')
 
 
-
 try:
+	#My credentials are saved in my python library so I can push this code to git without you seeing it.
 	import bot
-	USERNAME = bot.getuG()
-	PASSWORD = bot.getpG()
-	USERAGENT = bot.getaG()
+	USERNAME = bot.uG
+	PASSWORD = bot.pG
+	USERAGENT = bot.aG
 except:
 	pass
-
 r = praw.Reddit(USERAGENT)
 r.login(USERNAME, PASSWORD)
+
+
+mods = []
+#This will be filled on Refresh. Leave alone.
 
 def loadconfig():
 	global QUESTIONDELAY
@@ -50,14 +56,25 @@ def loadconfig():
 	#Number of posts to gather at a time. Praw can download 100 in a single request
 	REQUESTTAG = config['24']['REQUESTTAG']
 	#The tag in the title which identifies the post as a request
+	FLAIRPENDING = config['24']['FLAIRPENDING']
+	#Request flair: Pending. The request has been made but mods have not seen it yet
+	FLAIRDISCUSS = config['24']['FLAIRDISCUSS']
+	#Request flair: Discussing. The post has been xposted to relevant subreddits and members discuss the request
+	FLAIRAPPROVE = config['24']['FLAIRAPPROVE']
+	#Request flair: Approved. The community and moderators have approved the subreddit and will attempt to contact the Candidate
 
 def reqflair(post):
 	print('\tChecking request flair')
 	try:
 		pflair = post.link_flair_text
 	except:
-		pflair = ''
+		pflair = 'No Flair'
 	print('\t-' + pflair)
+	if pflair == 'No Flair':
+		print('\tApplying Flair: ' + FLAIRPENDING)
+		post.set_flair(FLAIRPENDING)
+		return True
+
 
 def scan():
 	print('Scanning ' + SUBREDDIT)
@@ -71,6 +88,38 @@ def scan():
 			if REQUESTTAG.lower() in post.title.lower():
 				reqflair(post)
 
+def inbox():
+	print('Scanning inbox')
+	pms = r.get_unread(unset_has_mail=True, update_user=True)
+	for pm in pms:
+		try:
+			mauthor = pm.author.name.lower()
+			mid = pm.id
+			print(mid)
+			if mauthor in mods:
+				print('\tFrom moderator')
+				subject = pm.subject.lower()
+				bodysplit = pm.body.lower().split('\n\n')
+				if subject == 'crosspost':
+					url = bodysplit[0]
+					subs = bodysplit[1:]
+					for m in range(len(subs)):
+						subs[m] = subs[m].replace('/r/','')
+
+		except:
+			pauthor = '[deleted]'
+
+
+def refresh():
+	#Must happen FIRST in the while loop, before any scans
+	#Refresh the list of moderators
+	global mods
+	subreddit = r.get_subreddit(SUBREDDIT)
+	moderators = subreddit.get_moderators()
+	mods = []
+	for moderator in moderators:
+		mods.append(moderator.name.lower())
 
 
 loadconfig()
+refresh()
---
+++ PointsBot/README.md
@@ -0,0 +1,12 @@
+PointsBot
+=========
+
+By saying a certain keyword, a user may give a "point" to the person who he replied to. Points are stored as flair. Here are some of the things the config can do:
+
+- Allow any user to distribute points, or only the OP of the thread
+
+- Allow only a certain number of points to be distributed in a single thread
+
+- Put users on an "exempt" list. Exempt users will not receive points. This is useful if they have special flair and you don't want to disrupt it
+
+[See here](http://www.reddit.com/r/GoldTesting/comments/2bzkp9/thing/)
