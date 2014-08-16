9fefad9bb6b81c55cef27df6efd51f7e3f4e4b6b
Voussoir <edalool@yahoo.com>
2014-08-16 12:34:53 -0700

    reddit 63
    We're licensed, brothers.

--- ScoreStats/scorestats.py
+++ ScoreStats/scorestats.py
@@ -16,7 +16,7 @@ sql = sqlite3.connect('sql.db')
 print('Loaded SQL Database')
 cur = sql.cursor()
 
-cur.execute('CREATE TABLE IF NOT EXISTS submissiondata(id TEXT, created INT, title TEXT, )')
+cur.execute('CREATE TABLE IF NOT EXISTS submissiondata(id TEXT, created INT, title TEXT, score INT)')
 cur.execute('CREATE TABLE IF NOT EXISTS commentdata(id TEXT)')
 cur.execute('CREATE TABLE IF NOT EXISTS oldlinks(ID TEXT)')
 print('Loaded Completed table')
