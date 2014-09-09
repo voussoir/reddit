a097d03cdda0012073021b91c8468a55d3355e2c
Voussoir <edalool@yahoo.com>
2014-09-09 01:54:31 -0700

    reddit 67
    Localizer can now create subfolders when it is downloading an album

--- Localize/localize.py
+++ Localize/localize.py
@@ -13,10 +13,10 @@ PASSWORD  = ""
 #This is the bot's Password. 
 USERAGENT = ""
 #This is a short description of what the bot does. For example "/u/GoldenSights' Newsletter Bot".
-SUBREDDIT = "CopperplateGothic"
+SUBREDDIT = "imgur"
 #This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subreddits, use "sub1+sub2+sub3+..."
 
-DOMAINS = ["imgur"]
+DOMAINS = ["imgur.com"]
 #Domains from which the bot will download images
 
 FILENAME = "_counter_ _subreddit_ _redditid_"
@@ -26,6 +26,7 @@ FILENAME = "_counter_ _subreddit_ _redditid_"
 #_redditid_ = The 6 digit ID of the reddit post
 #_imgurid_ = The 7 digit ID of the imgur image
 #_subreddit_ = The name of the subreddit it was posted in
+#_title_ = The title of the reddit post. Certain characters will be stripped for Windows reasons
 #_counter_ = A counter which will tick upward by 1 every time.
 
 FILEPATH = "Images"
@@ -35,7 +36,10 @@ FILEPATH = "Images"
 LEADINGZEROS = 3
 #If you use the counter, you may want the number to have leading zeros.
 
-MAXPOSTS = 10
+ALBUMSUBFOLDERS = True
+#When downloading an album, should all its images be put in a subfolder under the album's ID?
+
+MAXPOSTS = 100
 #This is how many posts you want to retrieve all at once. PRAW can download 100 at a time.
 WAIT = 20
 #This is how many seconds you will wait between cycles. The bot is completely inactive during this time.
@@ -94,41 +98,60 @@ def determinefiletype(path):
     else:
         return '.jpg'
 
-def readalbum(pid, purl, subreddit):
+def readalbum(pid, purl, subreddit, rtitle):
     #I DON'T KNOW WHAT I'M DOING
+    l = []
     print('Reading HTML for album page')
     reada = urllib.request.urlopen(purl)
     a = reada.readlines()
     reada.close()
-    l = []
     for line in a:
         line = str(line, encoding='utf-8')
-        if 'view full resolution' in line.lower():
-            l.append('http://i.imgur.com/' + line.strip().split('"')[1].split('/')[-1])
+        if '#' not in purl:
+            if 'view full resolution' in line.lower():
+                l.append('http://i.imgur.com/' + line.strip().split('"')[1].split('/')[-1])
+        else:
+            if 'data-src' in line.lower():
+                print('http://i.imgur.com/' + line.strip().split('"')[9].split('/')[-1].replace('s.', '.'))
+                l.append('http://i.imgur.com/' + line.strip().split('"')[9].split('/')[-1].replace('s.', '.'))
+
+
     print('Found ' + str(len(l)) + ' items')
+    name = purl.split('/')[-1].split('#')[0]
     for image in l:
         print('\tFound ' + image)
-        downloadimage(pid, image, subreddit, False)
+        downloadimage(pid, image, subreddit, False, rtitle, albumname=name)
 
 
 
-def downloadimage(pid, purl, subreddit, mustrename):
+def downloadimage(pid, purl, subreddit, mustrename, rtitle, albumname=""):
     global COUNTER
     fileextension = '.' + purl.split('.')[-1]
     imgurid = purl.split('/')[-1].replace(fileextension, '')
+    for char in ['\\', '/', ':', '*', '?', '"', '<', '>', '|']:
+        rtitle = rtitle.replace(char, '')
     filename = FILENAME
     filename = filename.replace('_counter_', ("%0" + str(LEADINGZEROS+1) + "d") % COUNTER)
     filename = filename.replace('_redditid_', pid)
     filename = filename.replace('_imgurid_', imgurid)
     filename = filename.replace('_subreddit_', subreddit)
+    filename = filename.replace('_title_', rtitle)
+
+    if ALBUMSUBFOLDERS == True and albumname != "":
+        if not os.path.exists(FILEPATH + '\\' + albumname):
+            os.makedirs(FILEPATH + '\\' + albumname)
+        filename = albumname + '\\' + filename
 
     fullpath = FILEPATH + "\\" +  filename + fileextension
 
     print('\tImgur ID:', imgurid, '|| Extension:', fileextension)
     print('\tDownloading image to', '"' + fullpath + '"\n')
 
-
-    urllib.request.urlretrieve(purl, fullpath)
+    try:
+        urllib.request.urlretrieve(purl, fullpath)
+    except Exception as e:
+        print('DOWNLOAD FAILED')
+        print(e)
 
     if mustrename == True:
         fileextension = determinefiletype(fullpath)
@@ -162,10 +185,10 @@ def scanSub():
                     mustrename = True
 
                 if 'imgur.com/a/' in purl:
-                    readalbum(pid, purl, post.subreddit.display_name)
+                    readalbum(pid, purl, post.subreddit.display_name, post.title)
 
                 else:
-                    downloadimage(pid, purl, post.subreddit.display_name, mustrename)
+                    downloadimage(pid, purl, post.subreddit.display_name, mustrename, post.title)
 
             cur.execute('INSERT INTO oldposts VALUES(?,?)', ['post', pid])
         sql.commit()
