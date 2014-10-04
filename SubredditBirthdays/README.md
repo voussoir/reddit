4d06ae49cae354ee20f25e38c434aab740a414cb
Voussoir <edalool@yahoo.com>
2014-10-03 23:14:05 -0700

    reddit 79
    Tons of SB Data. SB now tracks NSFW status (I had to requery the whole
    db for this). More sort modes for nsfw

--- SubredditBirthdays/README.md
+++ SubredditBirthdays/README.md
@@ -2,3 +2,5 @@ Subreddit Birthdays
 ==========
 
 Gathering some information on the creation dates of subreddits, so you don't have to.
+
+Once a subreddit is added to the list, it will never be re-queried. Subreddits which have become private or have changed their nsfw status will not be properly reflected here.
--- SubredditBirthdays/sb.py
+++ SubredditBirthdays/sb.py
@@ -25,7 +25,7 @@ except ImportError:
 
 sql = sqlite3.connect('sql.db')
 cur = sql.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS subreddits(ID TEXT, CREATED INT, HUMAN TEXT, NAME TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS subreddits(ID TEXT, CREATED INT, HUMAN TEXT, NSFW TEXT, NAME TEXT)')
 print('Loaded SQL Database')
 sql.commit()
 
@@ -91,7 +91,9 @@ def process(sr):
 		if not f:
 			h = human(sub.created_utc)
 			print('New: ' + sub.id + ' : ' + h + ' : ' + sub.display_name)
-			cur.execute('INSERT INTO subreddits VALUES(?, ?, ?, ?)', [sub.id, sub.created_utc, h, sub.display_name])
+			isnsfw = '1' if sub.over18 else '0'
+			isnsfw = 'NSFW:' + isnsfw
+			cur.execute('INSERT INTO subreddits VALUES(?, ?, ?, ?, ?)', [sub.id, sub.created_utc, h, isnsfw, sub.display_name])
 			sql.commit()
 		else:
 			olds += 1
@@ -116,6 +118,8 @@ def show():
 	filea = open('showt.txt', 'w')
 	fileb = open('showd.txt', 'w')
 	filec = open('shown.txt', 'w')
+	filed = open('shoxn.txt', 'w')
+	filee = open('shoxt.txt', 'w')
 	cur.execute('SELECT * FROM subreddits')
 	fetch = cur.fetchall()
 
@@ -123,11 +127,17 @@ def show():
 	print('Sorted by true time', file=filea)
 	for member in fetch:
 		print(str(member).replace("'", ''), file=filea)
+	shown(fetch, 'Sorted by nsfw by true time', filee)
+	filea.close()
+	filee.close()
 
-	fetch.sort(key=lambda x: x[3].lower())
+	fetch.sort(key=lambda x: x[4].lower())
 	print('Sorted by name', file=filec)
 	for member in fetch:
 		print(str(member).replace("'", ''), file=filec)
+	shown(fetch, 'Sorted by nsfw by name', filed)
+	filec.close()
+	filed.close()
 
 	l = list(fetch)
 	print(str(len(l)) + ' items.')
@@ -143,12 +153,33 @@ def show():
 	print('Sorted by day of month', file=fileb)
 	for member in l:
 		print(str(member).replace("'", ''), file=fileb)
-	filea.close()
 	fileb.close()
-	filec.close()
 
 
-def nearby(ranged=16):
+
+def shown(startinglist, header, fileobj):
+	nsfwyes = []
+	nsfwno = []
+	nsfwq = []
+	for item in startinglist:
+		if item[3] == 'NSFW:1':
+			nsfwyes.append(item)
+		elif item[3] == 'NSFW:?':
+			nsfwq.append(item)
+		else:
+			nsfwno.append(item)
+	print(header, file=fileobj)
+	for member in nsfwno:
+		print(str(member).replace("'", ''), file=fileobj)
+	print('•', file=fileobj)
+	for member in nsfwyes:
+		print(str(member).replace("'", ''), file=fileobj)
+	print('•', file=fileobj)
+	for member in nsfwq:
+		print(str(member).replace("'", ''), file=fileobj)
+
+
+def nearby(ranged=16, nsfwmode=2):
 	#find upcoming birthdays
 	cur.execute('SELECT * FROM subreddits')
 	fetched = cur.fetchall()
@@ -167,7 +198,7 @@ def nearby(ranged=16):
 
 		fetched[m] = member
 		
-	nowentry = ['Today', int("%0.0f" % now.timestamp()), datetime.datetime.strftime(now, "%m %d %Y %H:%M:%S UTC"), '##########']
+	nowentry = ['Today', int("%0.0f" % now.timestamp()), datetime.datetime.strftime(now, "%m %d %Y %H:%M:%S UTC"), 'NSFW:X', '##########']
 	fetched.append(nowentry)
 	fetched.sort(key=lambda x: (x[2][:6] + x[2][11:]))
 
@@ -175,9 +206,50 @@ def nearby(ranged=16):
 	results.append(nowentry)
 	results.append('')
 	for item in fetched[nowindex+1:nowindex+ranged+1]:
-		results.append(item)
+		if (item[3] == 'NSFW:1' and nsfwmode==1) or (item[3] == 'NSFW:0' and nsfwmode==0) or nsfwmode == 2:
+			results.append(item)
 
 
 	#return results
 	for item in results:
+		item = str(item)
+		item = item.replace("'", '')
 		print(item)
+
+
+def base36encode(number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+    """Converts an integer to a base36 string."""
+    if not isinstance(number, (int)):
+        raise TypeError('number must be an integer')
+    base36 = ''
+    sign = ''
+    if number < 0:
+        sign = '-'
+        number = -number
+    if 0 <= number < len(alphabet):
+        return sign + alphabet[number]
+    while number != 0:
+        number, i = divmod(number, len(alphabet))
+        base36 = alphabet[i] + base36
+    return sign + base36
+
+
+def base36decode(number):
+    return int(number, 36)
+
+def b36(i):
+	if type(i) == int:
+		return base36encode(i)
+	if type(i) == str:
+		return base36decode(i)
+
+def processir(startingpoint, ranger):
+	#Take subreddit ID as starting point and grab the next ranger items
+	if type(startingpoint) == str:
+		startingpoint = b36(startingpoint)
+	for pos in range(startingpoint, startingpoint+ranger):
+		newpoint = b36(pos).lower()
+		try:
+			processi(newpoint)
+		except:
+			pass
