befd977d39c5aae9c105048c9e011fe33fb20aec
Voussoir <edalool@yahoo.com>
2014-11-16 00:14:57 -0800

    reddit 96
    SchedulizerM is nearing completion

---
+++ Schedulizer-ModTeam/README.md
@@ -0,0 +1,97 @@
+SchedulizerM
+========
+
+Written for [/u/ApexRedditr](http://reddit.com/u/apexredditr) - [Req thread](http://www.reddit.com/r/RequestABot/comments/2m5abs/request_schedule_a_post/)
+
+Enormous overhaul of the Schedulizer system. This bot allows a team of moderators to put "Source Posts" in a centralized subreddit, and the content of those posts will immediately be dispersed at a desired time.
+
+#Creating a Schedule
+
+See screenshot 1
+
+A Source post **must** have three elements - A time to post, a subreddit to post, and a title to post, in that order. They must be demarcated by a Separator which is chosen by the bot owner, and is `||` by default. I advise you to never put the separator as part of your title, but you still may be able to.
+
+Additionally, if the bot is a moderator of the subreddit you are scheduling the post to, you may include the following flags in the subreddit section
+
+    [d] - Distinguish the post
+    [s] - Sticky the post
+    [f:Flair] - Assign the post with flair "Flair"
+    [fc:flairclass] - Assign the post with the CSS flair class "flairclass"
+
+Remember that the bot is the one making the post, not you! You will be notified when the post is created, but it will not otherwise be accredited to you. The bot must be a moderator of the chosen subreddit in order to distinguish the post, etc.
+
+You *can* tell the bot to schedule a post in the same subreddit that the schedules are being made in. Why would you do that, though?
+
+
+.
+
+#Editing Schedules
+
+See screenshot 2 and 3
+
+If the time, subreddit, or title of your source is invalid, it is considered a "critical error". You *must* edit this schedule. Even if a source is valid, you may edit it if you have changed your mind about something.
+
+To edit the schedule, simply reply to the bot's comment with the name of the key you wish to change, a colon, and the new value, like so:
+
+   Key: value
+
+You may only edit one key:value per line. See screenshot 3 for an example.
+
+There is some leniency in your key edits. For example, `flairtext: Heyo`, `flair-text: Heyo`, and `flair_text: Heyo` are all going to do the same thing. Here are the ways you can edit the values.
+
+	Time - 'time', 'timestamp'
+
+	Subreddit - 'reddit', 'subreddit', 'sr'
+	
+	Title  - 'title'
+
+	Distinguish - 'distinguish', 'dist', 'd'
+		'0', 'no', 'false', 'off'
+		'1', 'yes', 'true', 'on'
+
+	Sticky - 'sticky', 's'
+		'0', 'no', 'false', 'off'
+		'1', 'yes', 'true', 'on'
+
+	flair-text 'flair-text', 'flairtext', 'flair_text'
+
+	flair-css 'flair-css', 'flaircss', 'flair_css'
+
+
+.
+
+#Deleting Schedules
+
+Just delete your source post, and the bot will remove it from the calendar on the next cycle.
+
+
+.
+
+
+#Post content
+
+The Post will mimick the Source. Selftext if it was a selfpost, or URL if it was a linkpost. The selftext and url data is NOT collected until the post is actually being made, so you can edit the post content as much as you like without having to notify the bot.
+
+Click on any image to see the full size
+
+#####1 - A well-made post
+<p align="center">
+  <img src="https://github.com/voussoir/reddit/blob/master/.GitImages/SchedulizerM_00.png?raw=true" alt="SM"/>
+</p>
+
+
+.
+
+
+#####2 - Sometimes there are errors. Don't worry, you'll be notified and have plenty of time to fix things.
+<p align="center">
+  <img src="https://github.com/voussoir/reddit/blob/master/.GitImages/SchedulizerM_01.png?raw=true" alt="SM"/>
+</p>
+
+
+.
+
+#####3 - How to fix multiple errors at once
+<p align="center">
+  <img src="https://github.com/voussoir/reddit/blob/master/.GitImages/SchedulizerM_02.png?raw=true" alt="SM"/>
+</p>
--- Schedulizer-ModTeam/schedulizerm.py
+++ Schedulizer-ModTeam/schedulizerm.py
@@ -11,7 +11,7 @@ import re
 
 USERAGENT = ""
 #Describe the bot and what it does. Include your username
-USERNAME = "GoldenSights"
+USERNAME = "75000"
 #This is the bot's username
 PASSWORD = ""
 #This is the bot's password
@@ -25,12 +25,14 @@ TITLESEPARATOR = "||"
 #          ^Time to post        ^Sub to post    ^Title of post
 POSTEDFLAIR = "Post made!"
 #This flair will be assigned to the source when the post is made
-MAXPOSTS = 1
+MAXPOSTS = 3
 #The number of items you want to get from /new. Recommended 100
 ALLOWOTHEREDITS = False
 #Are users allowed to edit other peoples' post schedules?
+WAIT = 30
+#How many seconds in between loop cycles. Completely inactive during this time.
 
-POSTEDCOMMENT = "Your post to %s has been created. %s"
+POSTEDCOMMENT = "Your post to /r/%s has been created. %s"
 #Made in the source when the post is made
 
 FOOTER = """
@@ -69,27 +71,42 @@ Encountered the following errors:
 
 The post will use placeholder values until you correct the information
 
+_______
+
 """
 
-DISTINGUISHFAIL = "Attempted to distinguish post and failed."
+ERRORDISTINGUISHFAIL = "Attempted to distinguish post and failed."
+ERRORSTICKYFAIL = "Attempted to sticky post and failed."
+ERRORDATETIME = '!! DateTime: Could not understand time format, or date is invalid. You entered: `%s`'
+ERRORTOOEARLY = '!! DateTime: The time you have entered is before present time. You entered `%s`'
+ERRORTITLEFORM = '!! Title: Title expected 3 attributes separated by `' + TITLESEPARATOR + '`'
+ERRORLONGTITLE = "!! Title: Your title is too long. Max 300 characters, you have %d"
+ERRORSUBREDDIT = '!! Reddit: Subbreddit /r/%s could not be found'
+ERRORUNKNOWNCOMMAND = "Did not understand the command: `%s`"
+ERRORCRITICAL = '\n\nBecause of a critical post error, your chosen timestamp has been forfeited. You will need to edit it along with the other keys.\n\n'
+
+IMPOSSIBLETIME = 16756704000
 """ All done! """
 
 try:
 	import bot
 	#USERNAME = bot.uG
-	PASSWORD = bot.pG
-	USERAGENT = bot.aG
+	PASSWORD = bot.p7
+	USERAGENT = bot.a7
 except ImportError:
 	pass
 
-r = praw.Reddit(USERAGENT)
-r.login(USERNAME, PASSWORD)
-
+print('Loading database')
 sql = sqlite3.connect('sql.db')
 cur = sql.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS schedules(ID TEXT, TIME INT, REDDIT TEXT, TITLE TEXT, DIST INT, STICKY INT, FLAIR TEXT, FLCSS TEXT, POST TEXT)')
 #                                                   0         1          2            3          4         5           6           7           *
 sql.commit()
+
+print('Logging in')
+r = praw.Reddit(USERAGENT)
+r.login(USERNAME, PASSWORD)
+
 
 def getTime(bool):
 	timeNow = datetime.datetime.now(datetime.timezone.utc)
@@ -106,6 +123,7 @@ def processpost(inputpost):
 		inputpost = r.get_info(thing_id=inputpost)
 	sourceid = inputpost.id
 	print('Schedulizing post ' + sourceid)
+	nowstamp = getTime(True)
 	sourcetitle = inputpost.title
 	sourcesplit = sourcetitle.split(TITLESEPARATOR)
 	errors = []
@@ -121,20 +139,20 @@ def processpost(inputpost):
 		posttime = sourcesplit[0]
 		postsub = sourcesplit[1]
 		postsub = postsub.replace('/r/', '')
-		if '[d]' in postsub:
+		if '[d]' in postsub.lower():
 			dodist = 1
-		if '[s]' in postsub:
+		if '[s]' in postsub.lower():
 			dosticky = 1
-		regex = re.search("\[f:.*\]", postsub)
+
+		regex = re.search("\[f:[^\]]*\]", postsub, re.IGNORECASE)
 		if regex:
-			regex = regex.group(0)
-			postflair = regex
+			postflair = regex.group(0)
 			postflair = postflair[3:-1]
-		regex = re.search("\[fc:.*\]", postsub)
+
+		regex = re.search("\[fc:[^\]]*\]", postsub, re.IGNORECASE)
 		if regex:
-			regex = regex.group(0)
-			postflcss = regex
-			postflcss = postflcss[3:-1]
+			postflcss = regex.group(0)
+			postflcss = postflcss[4:-1]
 		elif postflair != "":
 			postflcss = removespecial(postflair)
 
@@ -142,30 +160,35 @@ def processpost(inputpost):
 		while '' in postsubsplit:
 			postsubsplit.remove('')
 		postsub = postsubsplit[0]
-		posttitle = sourcesplit[2]
+		posttitle = '||'.join(sourcesplit[2:])
 	except IndexError:
-		errors.append('! Title: Title expected 3 attributes separated by ' + TITLESEPARATOR)
+		errors.append(ERRORTITLEFORM)
 		critical = True
 
 	try:
 		posttimerender = dateparse(posttime)
 		posttimerender = posttimerender.replace(tzinfo=datetime.timezone.utc)
 		posttimestamp = posttimerender.timestamp()
+		if posttimestamp < nowstamp:
+			errors.append(ERRORTOOEARLY % posttime)
+			critical = True
 	except:
 		#December 31, 2500
-		posttimestamp = 16756704000
-		errors.append('! DateTime: Could not understand time format, or date is invalid. You entered: `' + posttime + '`')
+		posttimestamp = IMPOSSIBLETIME
+		errors.append(ERRORDATETIME % posttime)
 		critical = True
 
 	try:
-		r.get_subreddit(postsub, fetch=True)
+		validatesubreddit(postsub)
 	except:
-		errors.append('! Reddit: Subbreddit /r/' + postsub + ' could not be found')
+		errors.append(ERRORSUBREDDIT % postsub)
 		critical = True
 
 	#ID TEXT, TIME INT, REDDIT TEXT, TITLE TEXT, DIST INT, STICKY INT, FLAIR TEXT, FLCSS TEXT, POST TEXT
 	#  0         1          2            3          4         5           6           7           8
 
+	if critical:
+		posttimestamp = IMPOSSIBLETIME
 	datalist = [sourceid, posttimestamp, postsub, posttitle, dodist, dosticky, postflair, postflcss, "None"]
 	cur.execute('SELECT * FROM schedules WHERE ID=?', [sourceid])
 	fetch = cur.fetchone()
@@ -173,35 +196,123 @@ def processpost(inputpost):
 		cur.execute('INSERT INTO schedules VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)', datalist)
 		sql.commit()
 
-		schedulecomment = SCHEDULECOMMENT
-		if len(errors) > 0:
-			errors = "\n\n".join(errors)
-			schedulecomment = ERRORCOMMENT % errors
-		schedulecomment += buildtable(datalist)
-		schedulecomment += FOOTER
+		schedulecomment = buildcomment(datalist, errors, critical)
 		print('Writing comment')
 		inputpost.add_comment(schedulecomment)
 
-def updatepost(inputcomment):
-	sourceid = comment.link_id
-	print('Updating schedule for ' + sourceid + 'via comment ' + comment.id)
-	source = r.get_info(thing_id=sourcide)
+def updatepost(comment):
+	source = comment.submission
+	print('Updating schedule for ' + source.id + ' via comment ' + comment.id)
 	pauthor = source.author.name
 	cauthor = comment.author.name
 	if ALLOWOTHEREDITS or (pauthor == cauthor):
-		cur.execute('SELECT * FROM schedules WHERE ID=?', [sourceid])
+		cur.execute('SELECT * FROM schedules WHERE ID=?', [source.id])
 		data=cur.fetchone()
 		if data:
 			data= list(data)
 			errors = []
 			commentsplit = comment.body.split('\n')
+			while '' in commentsplit:
+				commentsplit.remove('')
 			for line in commentsplit:
 				line = line.split(':')
 				line[0] = line[0].replace(' ', '')
+				command = line[0].lower()
+				arg = ':'.join(line[1:])
+				if command in ['time', 'timestamp']:
+					try:
+						posttimerender = dateparse(arg)
+						posttimerender = posttimerender.replace(tzinfo=datetime.timezone.utc)
+						posttimestamp = posttimerender.timestamp()
+					except:
+						#December 31, 2500
+						posttimestamp = IMPOSSIBLETIME
+						errors.append(ERRORDATETIME % posttime)
+					data[1] = posttimestamp
 
+				elif command in ['reddit', 'subreddit', 'sr']:
+					try:
+						arg = arg.replace(' ', '')
+						arg=arg.replace('/r/', '')
+						validatesubreddit(arg)
+					except:
+						#This will be errored in the upcoming `ispostvalid` line
+						pass
+					data[2] = arg
+				elif command in ['title']:
+					data[3] = arg
+				elif command in ['distinguish', 'dist', 'd']:
+					if arg.lower() in ['0', 'no', 'false', 'off']:
+						arg = 0
+					if arg.lower() in ['1', 'yes', 'true', 'on']:
+						arg = 1
+					data[4] = arg
+				elif command in ['sticky', 's']:
+					if arg.lower() in ['0', 'no', 'false', 'off']:
+						arg = 0
+					if arg.lower() in ['1', 'yes', 'true', 'on']:
+						arg = 1
+					data[5] = arg
+				elif command in ['flair-text', 'flairtext', 'flair_text']:
+					data[6] = arg
+				elif command in ['flair-css', 'flaircss', 'flair_css']:
+					data[7] = removespecial(arg)
+				else:
+					errors.append(ERRORUNKNOWNCOMMAND % command)
+
+		print('\tChecking schedule validity')
+		status = ispostvalid(data, errors)
+		if status[0] == False:
+			data[1] = IMPOSSIBLETIME
+			critical = True
+		else:
+			critical = False
+		schedulecomment = buildcomment(data[:], errors, critical)
+		print('\tWriting comment')
+		comment.reply(schedulecomment)
+		cur.execute('DELETE FROM schedules WHERE ID=?', [source.id])
+		cur.execute('INSERT INTO schedules VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)', data)
+		sql.commit()
+		print('\tDone.')
 	else:
 		print(cauthor + ' may not edit ' + pauthor + "'s post")
 
+def validatesubreddit(sr):
+	#This will intentionall crash if /r/sr does not exist
+	sr = sr.replace('/r/', '')
+	r.get_subreddit(sr, fetch=True)
+
+def ispostvalid(inputdata, errors):
+	nowstamp = getTime(True)
+	status = True
+	if inputdata[1] < nowstamp:
+		n = datetime.datetime.utcfromtimestamp(inputdata[1])
+		n = datetime.datetime.strftime(n, "%B %d %Y %H:%M")
+		errors.append(ERRORTOOEARLY % n)
+		status = False
+	try:
+		validatesubreddit(inputdata[2])
+	except:
+		print('\tBad subreddit: ' + inputdata[2])
+		errors.append(ERRORSUBREDDIT % inputdata[2])
+		status = False
+	if len(inputdata[3]) > 300:
+		errors.append(ERRORLONGTITLE % len(inputdata[3]))
+		status = False
+	return [status, errors]
+
+def buildcomment(datalist, errors, critical=False):
+	schedulecomment = SCHEDULECOMMENT
+	if len(errors) > 0:
+		errors = "\n\n".join(errors)
+		schedulecomment = ERRORCOMMENT % errors
+	if critical:
+		schedulecomment += ERRORCRITICAL
+	schedulecomment += buildtable(datalist)
+	schedulecomment += FOOTER
+	return schedulecomment
+#ID TEXT, TIME INT, REDDIT TEXT, TITLE TEXT, DIST INT, STICKY INT, FLAIR TEXT, FLCSS TEXT, POST TEXT
+#  0         1          2            3          4         5           6           7           8
 def buildtable(inputdata):
 	timeobj = datetime.datetime.utcfromtimestamp(inputdata[1])
 	inputdata[1] = datetime.datetime.strftime(timeobj, "%B %d %Y %H:%M UTC")
@@ -224,14 +335,14 @@ def buildtable(inputdata):
 	return table
 
 def removespecial(inputstr):
-	ok = string.ascii_letters + ascii_digits
+	ok = string.ascii_letters + string.digits
 	outstr = "".join([x for x in inputstr if x in ok])
+	return outstr
 
 def manage_new():
 	print('Managing ' + SUBREDDIT + '/new')
 	subreddit = r.get_subreddit(SUBREDDIT)
 	new = list(subreddit.get_new(limit=MAXPOSTS))
-	now = getTime(True)
 	for post in new:
 		pid = post.id
 		cur.execute('SELECT * FROM schedules WHERE ID=?', [pid])
@@ -240,15 +351,106 @@ def manage_new():
 
 def manage_unread():
 	print('Managing inbox')
-	inbox = list(r.get_unread(limit=None))
+	inbox = list(r.get_unread(limit=100))
 	for message in inbox:
 		if isinstance(message, praw.objects.Message):
 			if message.subject.lower() == "ping":
 				message.reply("Pong")
-		elif isinstance(message, praw.bojects.Comment):
-			commentsub = comment.subreddit.display_name
+				print('Responding to ping')
+		elif isinstance(message, praw.objects.Comment):
+			commentsub = message.subreddit.display_name
 			if commentsub.lower() == SUBREDDIT.lower():
-				updatepost(comment)
+				updatepost(message)
 
 		message.mark_as_read()
-manage_new()
+
+def manage_schedule():
+	print('Managing schedules')
+	cur.execute('SELECT * FROM schedules WHERE POST =?', ['None'])
+	fetch = cur.fetchall()
+	fetch = list(fetch)
+	fetch.sort(key=lambda x: x[1])
+
+	reread = False
+	idlist = ['t3_'+i[0] for i in fetch]
+	submissionlist = []
+	print('Checking for deletions')
+	while len(idlist) > 0:
+		submissionlist += r.get_info(thing_id=idlist[:100])
+		idlist = idlist[100:]
+	for item in submissionlist:
+		if not item.author:
+			print('\t' + item.id + ' has been deleted')
+			cur.execute('DELETE FROM schedules WHERE ID=?', [item.id])
+			sql.commit()
+			reread = True
+
+	if reread:
+		cur.execute('SELECT * FROM schedules WHERE POST =?', ['None'])
+		fetch = cur.fetchall()
+		fetch = list(fetch)
+		fetch.sort(key=lambda x: x[1])
+
+
+	nowstamp = getTime(True)
+	for schedule in fetch:
+		postid = schedule[0]
+		print('Checking schedule ' + postid, end="")
+		posttime = int(schedule[1])
+		if posttime < nowstamp:
+			print()
+			print('\tPreparing to post')
+			post = r.get_info(thing_id="t3_" + postid)
+			ptitle = schedule[3]
+			psub = schedule[2]
+			print('\tSubmitting post')
+			if post.is_self:
+				pbody = post.selftext
+				newpost = r.submit(psub, ptitle, text=pbody)
+			else:
+				purl = post.url
+				newpost = r.submit(psub, ptitle, url=purl, resubmit=True)
+			errors = []
+			if schedule[4] == 1:
+				try:
+					print('\tDistinguishing')
+					newpost.distinguish()
+				except:
+					print('\tDistinguish failed')
+					errors.append(ERRORDISTINGUISH)
+			if schedule[5] == 1:
+				try:
+					print('\tStickying')
+					newpost.sticky()
+				except:
+					print('\tSticky failed')
+					errors.append(ERRORSTICKYFAIL)
+			if schedule[6] != "" or schedule[7] != "":
+				try:
+					print('\tFlairing')
+					newpost.set_flair(flair_text=schedule[6], flair_css_class=schedule[7])
+				except:
+					print('\tFlair failed')
+			newsub = newpost.subreddit.display_name
+			newlink = newpost.short_link
+			newid = newpost.id
+			newcomment = POSTEDCOMMENT % (newsub, newlink)
+			newcomment += '\n\n'.join(errors)
+			cur.execute('UPDATE schedules SET POST=? WHERE ID=?', [newid, postid])
+			sql.commit()
+			post.add_comment(newcomment)
+
+		else:
+			print(" : T-" + str(round(posttime - nowstamp)))
+
+
+
+while True:
+	try:
+		manage_new()
+		manage_unread()
+		manage_schedule()
+	except Exception as e:
+		print("ERROR!!!", e)
+	print("Sleeping\n")
+	time.sleep(WAIT)
