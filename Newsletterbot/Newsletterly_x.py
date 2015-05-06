c51a4f38dbf57a3c30555cff8a4fff495a21b537
Voussoir <edalool@yahoo.com>
2015-05-05 20:14:17 -0700

    reddit 121
    Newsletterly overhaul again.

--- Newsletterbot/Newsletterly_x.py
+++ Newsletterbot/Newsletterly_x.py
@@ -28,7 +28,7 @@ MESSAGE_SUBREDDIT_FAIL = """
 Could not find /r/%s. Make sure it's spelled correctly and is a
 public subreddit (Or add /u/Newsletterly as a contributor).
 """
-MESSAGE_UNSUSBSCRIBE = "You have unsubscribed from /r/%s"
+MESSAGE_UNSUBSCRIBE = "You have unsubscribed from /r/%s"
 MESSAGE_UNSUBSCRIBE_ALREADY = "You are not currently subscribed to /r/%s"
 MESSAGE_UNSUBSCRIBE_FORCE = "You have forcefully removed /u/%s from /r/%s"
 MESSAGE_HEADER = "Your subscribed subreddits have had some new posts:"
@@ -63,13 +63,13 @@ except ImportError:
 
 WAITS = str(WAIT)
 
-sql = sqlite3.connect('sql.db')
-print('Loaded SQL Database')
+sql = sqlite3.connect('newsletterly.db')
 cur = sql.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
-print('Loaded Completed table')
+cur.execute('CREATE INDEX IF NOT EXISTS oldpostindex ON oldposts(ID)')
 cur.execute('CREATE TABLE IF NOT EXISTS subscribers(name TEXT, reddit TEXT)')
-print('Loaded Subscriber table')
+cur.execute('CREATE TABLE IF NOT EXISTS spool(name TEXT, message TEXT)')
+print('Loaded SQL Database')
 
 sql.commit()
 
@@ -77,6 +77,13 @@ print('Logging in')
 r = praw.Reddit(USERAGENT)
 r.login(USERNAME, PASSWORD) 
 del PASSWORD
+
+def fetchgenerator():
+	while True:
+		fetch = cur.fetchone()
+		if fetch is None:
+			break
+		yield fetch
 
 def get_subscription_reddits(user=None, join=True):
 	# Get a multi-subreddit ("a+b+c") for user or all users
@@ -90,8 +97,7 @@ def get_subscription_reddits(user=None, join=True):
 	fetch = list(set(fetch))
 	if not join:
 		return fetch
-	fetch = "+".join(fetch)
-	return fetch
+	return "+".join(fetch)
 
 def add_subscription(user, subreddit):
 	user = user.lower()
@@ -118,20 +124,20 @@ def drop_subscription(user, subreddit):
 	subreddit = subreddit.lower()
 	if subreddit == 'all':
 		cur.execute('DELETE FROM subscribers WHERE LOWER(name)=?', [user])
-		return (MESSAGE_USUBSCRIBE % subreddit)
+		return (MESSAGE_UNSUBSCRIBE % subreddit)
 	cur.execute('SELECT * FROM subscribers WHERE LOWER(name)=? AND LOWER(reddit)=?', 
 				[user, subreddit])
 	if cur.fetchone():
 		cur.execute('DELETE FROM subscribers WHERE LOWER(reddit)=?', [subreddit])
 		sql.commit()
-		return (MESSAGE_UNSUSBSCRIBE % subreddit)
+		return (MESSAGE_UNSUBSCRIBE % subreddit)
 	else:
 		return (MESSAGE_UNSUBSCRIBE_ALREADY % subreddit)
 
 def count_subscriptions():
-	cur.execute('SELECT * FROM subscribers')
-	fetch = cur.fetchall()
-	return len(fetch)
+	cur.execute('SELECT COUNT(*) FROM subscribers')
+	count = cur.fetchone()[0]
+	return count
 
 def format_post(submission):
 	template = MESSAGE_POSTFORMAT
@@ -148,63 +154,113 @@ def format_post(submission):
 	template = '[' + template + '](' + link + ')'
 	return template
 
-def manage():
+def add_to_spool(user, message, nosave=False):
+	if isinstance(user, praw.objects.Redditor):
+		user = user.name
+	user = user.lower()
+	cur.execute('SELECT * FROM spool WHERE name=? AND message=?', [user, message])
+	if cur.fetchone():
+		raise Exception("Message already exists in spool")
+	cur.execute('INSERT INTO spool VALUES(?, ?)', [user, message])
+	print('\tadded %s to spool' % user)
+	if nosave is False:
+		sql.commit()
+
+def get_from_spool():
+	cur.execute('SELECT ROWID, * FROM spool')
+	return cur.fetchone()
+
+def drop_from_spool(spool, nosave=False):
+	cur.execute('DELETE FROM spool WHERE ROWID=?', [spool[0]])
+	print('\tdropped %s from spool' % spool[1])
+	if nosave is False:
+		sql.commit()
+
+def manage_spool():
+	print('Managing spool')
+	if NOSEND:
+		return
+	while True:
+		spoolmessage = get_from_spool()
+		if spoolmessage is None:
+			break
+		rowid = spoolmessage[0]
+		user = spoolmessage[1]
+		final = spoolmessage[2]
+		print('Sending Newsletter to %s' % user)
+		try:
+			r.send_message(user, MESSAGE_SUBJECT, final, captcha=None)
+		except praw.errors.InvalidUser:
+			drop_subscription(user, 'all')
+			r.send_message(ADMIN, 'invalid user', user, captcha=None)
+		drop_from_spool(spoolmessage)
+		sql.commit()
+
+def manage_inbox():
 	print('Checking Inbox')
-	pms = r.get_unread(unset_has_mail=True, update_user=True)
+	pms = list(r.get_unread(limit=None))
+	# Remove unread from cache to guarantee every fetch is fresh.
+	r.evict(r.config['unread'] + '.json')
 	for pm in pms:
+		pm.mark_as_read()
 		interpretation = interpret_message(pm)
 		if interpretation:
 			print('\tReplying...')
 			pm.reply(interpretation)
-		pm.mark_as_read()
-	sql.commit()
 
+def manage_posts():
 	print('Finding posts')
+	usersubs = {}
 	cur.execute('SELECT * FROM subscribers')
-	userfetch = cur.fetchall()
-	userfetch = [f[0].lower() for f in userfetch]
-	userfetch = list(set(userfetch))
-
-	cur.execute('SELECT * FROM oldposts')
-	oldposts = cur.fetchall()
-	oldposts = [f[0] for f in oldposts]
+	for user in fetchgenerator():
+		name = user[0].lower()
+		sub = user[1]
+		if name not in usersubs:
+			usersubs[name] = set()
+		usersubs[name].add(sub)
 
 	postlist = set()
-
-	for user in userfetch:
+	for user in usersubs:
+		# In this first block, go through each subscriber and get posts on their
+		# subreddits. Reassign the results of this scan to the usersubs dict.
+		# If a server timeout causes this portion to crash, nothing
+		# of value is lost because we don't send mail or write to the db yet,
+		# so no duplicate mail will be created.
+		subreddits = '+'.join(usersubs[user])
 		print('Finding posts for %s' % user)
-		usersubs = get_subscription_reddits(user)
-		print('\t/r/' + usersubs)
-		subreddit = r.get_subreddit(usersubs)
-		posts = subreddit.get_new(limit=MAXPOSTS)
+		print('\t/r/' + subreddits)
+		subreddit = r.get_subreddit(subreddits)
+		posts = list(subreddit.get_new(limit=MAXPOSTS))
 		results = []
 		for post in posts:
-			if post.id not in oldposts:
-				print('\t' + post.id)
-				results.append(post)
+			cur.execute('SELECT * FROM oldposts WHERE id=?', [post.id])
+			if not cur.fetchone():
 				postlist.add(post.id)
+				if len(results) < MAX_PER_MESSAGE:
+					print('\t' + post.id)
+					results.append(post)
 		results = [format_post(f) for f in results]
+		usersubs[user] = results
 
+	for postid in postlist:
+		cur.execute('INSERT INTO oldposts VALUES(?)', [postid])
+	sql.commit()
+	print()
+	for user in usersubs:
+		# In this second block, use the subscription results to construct a PM
+		# and insert the PM to the spool to be sent later. By spooling the messages,
+		# we make sure that no api errors cause us to lose any results.
 		if len(results) > 0:
 			final = MESSAGE_HEADER + '\n\n'
 			final += '\n\n'.join(results)
 			final += '\n\n' + MESSAGE_FOOTER
 			if NOSEND:
-				print('NO SEND')
+				print('%s NO SEND' % user)
 			else:
-				try:
-					r.send_message(user, MESSAGE_SUBJECT, final, captcha=None)
-				except praw.errors.InvalidUser:
-					drop_subscription(user, 'all')
-					r.send_message(ADMIN, 'invalid user', user, captcha=None)
-				except requests.exceptions.HTTPError:
-					pass
+				add_to_spool(user, final, nosave=True)
+				sql.commit()
 		else:
-			print('\tNone')
-
-	for postid in postlist:
-		cur.execute('INSERT INTO oldposts VALUES(?)', [postid])
-	sql.commit()
+			print('%s None' % user)
 
 def interpret_message(pm):
 	results = []
@@ -215,7 +271,9 @@ def interpret_message(pm):
 		results = MESSAGE_TOOLONG + MESSAGE_FOOTER
 		return results
 	for line in bodysplit:
-		linesplit = line.split()
+		linesplit = line.replace(', ', ' ')
+		linesplit = linesplit.replace(',', ' ')
+		linesplit = linesplit.split(' ')
 		try:
 			command = linesplit[0]
 			command = command.replace(',', '')
@@ -231,15 +289,7 @@ def interpret_message(pm):
 			argument = argument.replace(' ', '')
 			print("%s : %s - %s" % (author, command, argument))
 
-			if command == 'subscribe':
-				status = add_subscription(author, argument)
-				results.append(status)
-
-			elif command == 'unsubscribe':
-				status = drop_subscription(author, argument)
-				results.append(status)
-
-			elif command == 'report':
+			if command == 'report':
 				status = MESSAGE_REPORT_REQUEST + '\n\n'
 				status += build_report(author)
 				results.append(status)
@@ -247,6 +297,24 @@ def interpret_message(pm):
 			elif command == 'reportall' and author == ADMIN:
 				status = MESSAGE_REPORT_ALL + '\n\n'
 				status += build_report(None, supermode=True)
+				results.append(status)
+
+			elif command == 'kill' and author == ADMIN:
+				pm.mark_as_read()
+				r.send_message(ADMIN, "force kill", "bot is being turned off")
+				quit()
+
+			if not argument:
+				# If we've reached an argument-based function with
+				# no argument, skip to the next arg.
+				continue
+
+			if command == 'subscribe':
+				status = add_subscription(author, argument)
+				results.append(status)
+
+			elif command == 'unsubscribe':
+				status = drop_subscription(author, argument)
 				results.append(status)
 
 			elif command == 'reportuser' and author == ADMIN:
@@ -258,22 +326,17 @@ def interpret_message(pm):
 				argument = argument.split('.')
 				user = argument[0]
 				subreddit = argument[1]
+				add_subscription(user, subreddit)
 				status = (MESSAGE_SUBSCRIBE_FORCE % (user, subreddit)) + '\n\n'
-				status += add_subscription(user, subreddit)
 				results.append(status)
 
 			elif command == 'forceunsubscribe' and author == ADMIN:
 				argument = argument.split('.')
 				user = argument[0]
 				subreddit = argument[1]
+				drop_subscription(user, subreddit)
 				status = (MESSAGE_UNSUBSCRIBE_FORCE % (user, subreddit)) + '\n\n'
-				status += drop_subscription(user, subreddit)
 				results.append(status)
-
-			if command == 'kill' and author == ADMIN:
-				pm.mark_as_read()
-				r.send_message(ADMIN, "force kill", "bot is being turned off")
-				quit()
 
 	if len(results) > 0:
 		results = '\n\n_____\n\n'.join(results)
@@ -285,7 +348,8 @@ def interpret_message(pm):
 	return None
 
 def build_report(user, supermode=False):
-	userlist = [user]
+	if isinstance(user, str):
+		userlist = [user]
 	results = []
 	if supermode:
 		cur.execute('SELECT * FROM subscribers')
@@ -297,7 +361,7 @@ def build_report(user, supermode=False):
 		results.append(status)
 	for user in userlist:
 		status = get_subscription_reddits(user, join=False)
-		status.append(get_subscription_reddits(user, join=True))
+		status.append('+'.join(status))
 		status = ['/r/' + f for f in status]
 		status[-1] = "All: " + status[-1]
 		status = '\n\n'.join(status)
@@ -309,7 +373,12 @@ def build_report(user, supermode=False):
 
 while True:
 	try:
-		manage()
+		manage_inbox()
+		print('--')
+		manage_posts()
+		print('--')
+		manage_spool()
+		print('--')
 	except Exception:
 		traceback.print_exc()
 	print('%d active subscriptions' % count_subscriptions())
