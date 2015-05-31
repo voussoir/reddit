b7f27a6f8d5f64219392925e9c745676b6e61c31
Voussoir <edalool@yahoo.com>
2015-05-31 01:37:07 -0700

    reddit 122
    Created alternate NFR function. The first one was not working properly
    with generators and lazyloading data.

--- Overlap/overlap.py
+++ Overlap/overlap.py
@@ -1,11 +1,16 @@
 import praw
 import traceback
 import time
+import types
 
 ''' CONFIG '''
 
 USERAGENT = ''
 MAXPOSTS = 100
+
+DROPOUT = [404]
+# This error code will cause the nofailrequest to exit.
+# There's no reason to repeatedly request a 404.
 
 ''' END CONFIG '''
 
@@ -17,6 +22,28 @@ except ImportError:
 
 r = praw.Reddit(USERAGENT)
 
+def nfr2(function, *fargs, **fkwargs):
+	while True:
+		try:
+			results = function(*fargs, **fkwargs)
+			if isinstance(results, types.GeneratorType):
+				results = list(results)
+			return results
+		except praw.requests.exceptions.HTTPError as e:
+			if e.response.status_code == DROPOUT:
+				return None
+			if isinstance(DROPOUT, list) and e.response.status_code in DROPOUT:
+				return None
+			traceback.print_exc()
+			print('Retrying in 2...')
+			time.sleep(2)
+		except KeyboardInterrupt:
+			raise Exception("CTRL+C")
+		except:
+			traceback.print_exc()
+			print('Retrying in 2...')
+			time.sleep(2)
+
 def nfr(function, dropout=None):
 	'''
 	"No Fail Request"
@@ -25,45 +52,53 @@ def nfr(function, dropout=None):
 	version of that function that will try-catch, sleep, and loop until it
 	finally returns.
 	'''
+	def b():
+		traceback.print_exc()
+		print('Retrying in 2...')
+		time.sleep(2)
 	def a(*args, **kwargs):
 		while True:
 			try:
-				try:
-					result = function(*args, **kwargs)
-					return result
-				except praw.requests.exceptions.HTTPError as e:
-					if dropout is None:
-						raise e
-					if e.response.status_code == dropout:
-						return None
-					if isinstance(dropout, list) and e.response.status_code in dropout:
-						return None
-					else:
-						raise e
-				except AssertionError:
-					# Strange PRAW bug causes certain MoreComments
-					# To throw assertion error, so just ignore it
-					# And get onto the next one.
-					return []
+				result = function(*args, **kwargs)
+				return result
+			except praw.requests.exceptions.HTTPError as e:
+				if e.response.status_code == dropout:
+					return None
+				if isinstance(dropout, list) and e.response.status_code in dropout:
+					return None
+				else:
+					b()
+			except requests.exceptions.ConnectionError:
+				b()
+			except AssertionError:
+				# Strange PRAW bug causes certain MoreComments
+				# To throw assertion error, so just ignore it
+				# And get onto the next one.
+				return []
+			except KeyboardInterrupt:
+				raise Exception("KeyboardInterrupt")
 			except:
-				traceback.print_exc()
-				print('Retrying in 2...')
-				time.sleep(2)
+				b()
 	return a
 
-def process_subreddit(sr):
+def get_subreddit_authors(sr):
 	sr = sr.lower()
 	subreddit = nfr(r.get_subreddit)(sr)
 	print('/r/%s/new' % sr)
-	posts = list(nfr(subreddit.get_new)(limit=MAXPOSTS))
+	#posts = list(nfr(subreddit.get_new)(limit=MAXPOSTS))
+	posts = nfr2(subreddit.get_new, limit=MAXPOSTS)
 	print('/r/%s/comments' % sr)
-	posts += list(nfr(subreddit.get_comments)(limit=MAXPOSTS))
+	#posts += list(nfr(subreddit.get_comments)(limit=MAXPOSTS))
+	posts += nfr2(subreddit.get_comments, limit=MAXPOSTS)
 
 	authors = [post.author.name for post in posts if post.author is not None]
-	authors = set(authors)
-	count = len(authors)
-	print('Found %d authors' % count)
+	authors = list(set(authors))
+	authors.sort(key=lambda x: x.lower())
+	print('Found %d authors' % len(authors))
+	return authors
 
+def process_userlist(authors):
+	count = len(authors)
 	i = 1
 	userreddits = {}
 	totalreddits = {}
@@ -82,17 +117,23 @@ def process_subreddit(sr):
 	totalreddits['%totalsubs%'] = (len(totalreddits) - 1)
 	return totalreddits
 
+def process_subreddit(sr):
+	authors = get_subreddit_authors(sr)
+	results = process_userlist(authors)
+	return results
+
 def process_user(username, pre=''):
 	user = nfr(r.get_redditor, dropout=404)(username)
 	if user is None:
 		return {}
 	print('\t%s/u/%s/submitted' % (pre, username))
-	userposts = list(nfr(user.get_submitted)(limit=MAXPOSTS))
+	#userposts = list(nfr(user.get_submitted)(limit=MAXPOSTS))
+	userposts = nfr2(user.get_submitted, limit=MAXPOSTS)
 	print('\t%s/u/%s/comments' % (pre, username))
-	userposts += list(nfr(user.get_comments)(limit=MAXPOSTS))
+	#userposts += list(nfr(user.get_comments)(limit=MAXPOSTS))
+	userposts += nfr2(user.get_comments, limit=MAXPOSTS)
 
 	userreddits = {'%totalposts%':len(userposts)}
-	#print(userreddits)
 	for post in userposts:
 		subreddit = post.subreddit.display_name.lower()
 		userreddits[subreddit] = userreddits.get(subreddit, 0) + 1
