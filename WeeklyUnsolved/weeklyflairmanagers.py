da4298f345674d8c5b89b02a76a0dc4347a2c43f
Voussoir <edalool@yahoo.com>
2015-08-23 23:36:11 -0700

    reddit 125 - the oauthening
    rework according to flair needs

--- WeeklyUnsolved/weeklyflairmanagers.py
+++ WeeklyUnsolved/weeklyflairmanagers.py
@@ -6,6 +6,9 @@ warnings.filterwarnings('ignore')
 print('logging in')
 r=bot.oG()
 
+FLAIR_UNSOLVED = 'unsolved'
+MAX_ROOT_COMMENTS = 1
+MAX_TOTAL_COMMENTS = 24
 IGNORE_DELETED_AUTHORS = True
 SAVE_TO_TXT = 'results_%Y%b%d.txt'
 
@@ -33,23 +36,24 @@ for submissionindex, submission in enumerate(new):
             break
         continue
     old_in_a_row = 0
-
-    if IGNORE_DELETED_AUTHORS and submission.author is None:
+    if submission.link_flair_text != FLAIR_UNSOLVED:
         continue
 
-    if submission.link_flair_text not in ['unsolved', 'Waiting on OP']:
+    if IGNORE_DELETED_AUTHORS and submission.author is None:
         continue
 
     # make sure to perform this part AS LATE AS POSSIBLE to avoid
     # api calls.
     submission.replace_more_comments(limit=None, threshold=1)
+    roots = submission.comments[:]
     total = praw.helpers.flatten_tree(submission.comments)
+    submission.croots = roots
     submission.ctotal = total
-    
-    if submission.link_flair_text == 'unsolved' and len(total) != 0:
+    if len(total) > MAX_TOTAL_COMMENTS:
         continue
-
-    if submission.link_flair_text == 'Waiting on OP' and len(total) != 1:
+    
+    # only counts roots
+    if len(roots) > MAX_ROOT_COMMENTS:
         continue
 
     results.append(submission)
