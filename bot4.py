8f49d735f8e204c4e71efaf7fa08cc6ff9ff6f1b
Ethan Dalool <git@voussoir.net>
2021-12-02 11:01:54 -0800

    Add more instructions to bot4.py.

--- bot4.py
+++ bot4.py
@@ -39,15 +39,45 @@ with different names, and then using `import specialbot as bot` within the
 application, so that the rest of the interface can stay the same.
 '''
 
-import praw
-
-# https://praw.readthedocs.io/en/latest/tutorials/refresh_token.html
-CONTACT_INFO = ''
+# The USERAGENT is a short description of why you're using reddit's API.
+# You can make it as simple as "/u/myusername's bot for /r/mysubreddit".
+# It should include your username so that reddit can contact you if there
+# is a problem.
 USERAGENT = 'xxx'
+
+# CONTACT_INFO can be your reddit username, or your email address, or any other
+# means of contacting you. This is used for some bot programs but not all.
+CONTACT_INFO = ''
+
+# It's time to get your OAuth credentials.
+#  1. Go to https://old.reddit.com/prefs/apps
+#  2. Click create a new app
+#  3. Give it any name you want
+#  4. Choose "script" type
+#  5. Description and About URI can be blank
+#  6. Put "http://localhost:8080" as the Redirect URI
+#  7. Now that you have created your app, write down the app ID (which appears
+#     under its name), the secret, and the URI (http://localhost:8080) in the
+#     variables below:
 APP_ID = 'xxx'
 APP_SECRET = 'xxx'
 APP_URI = 'xxx'
+#  8. Go to https://praw.readthedocs.io/en/latest/tutorials/refresh_token.html#obtaining-refresh-tokens
+#  9. Copy that script and save it to a .py file on your computer
+# 10. The instructions at the top of the script tell you to run two "EXPORT"
+#     commands before running the script. This only works on Unix. If you are on
+#     Windows, or simply don't want to bother with environment variables, ignore
+#     that part of the instructions and instead add `client_id='XXXX'` and
+#     `client_secret='XXXX'` into the praw.Reddit constructor that you see on
+#     line 40 of that script. When I say XXXX I mean the values you just wrote
+#     down.
+# 11. Run the script on your command line `python obtain_refresh_token.py`
+# 12. Write down the refresh token that it gives you:
 APP_REFRESH = 'xxx'
+
+################################################################################
+
+import praw
 
 def anonymous():
     r = praw.Reddit(
